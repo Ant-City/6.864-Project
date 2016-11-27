6ee436e425ffb8946014e3580e062256cb058c7c
Henry Aspegren <henrya@mit.edu>
2016-11-27 17:15:00 -0500

    modified to re train embeddings using only the cleaned files

--- corpusreader.py
+++ corpusreader.py
@@ -17,8 +17,10 @@ class EntireCorpus(object):
 			if not os.path.isdir(bookfile_dir):
 				continue
 			for textfile_name in os.listdir(bookfile_dir):
-				# only use the .txt files, not the csv metadata)
-				if textfile_name[-3:] == "txt":
+				# only use the cleaned .txt files, not the csv metadata or uncleaned files
+				if len(textfile_name) < 12:
+					continue
+				if textfile_name[-12:] == "_cleaned.txt":
 					textfile = os.path.join(bookfile_dir, textfile_name)
 					# this is used to 
 					with codecs.open(textfile, mode='r', encoding='utf-8', errors='ignore') as fp:
@@ -57,3 +59,5 @@ def textToSentences(text):
 # corpus_generator = EntireCorpus('Books/')
 
 # model = gensim.models.Word2Vec(corpus_generator, min_count=10)
+
+
--- helper_utils/renaming_script.py
+++ helper_utils/renaming_script.py
@@ -4,49 +4,10 @@ import re
 import shutil
 from tempfile import NamedTemporaryFile
 
-#directory = 'Books/'
+root_dir = 'Books/'
 
-
-directories = [	'Books/A_Midsummer_Nights_Dream/',
-	'Books/Alls_Well_That_Ends_Well/',
-	'Books/Antony_and_Cleopatra/',
-	'Books/As_You_Like_It/',
-	'Books/Coriolanus/',
-	'Books/Cymbeline/',
-	'Books/Hamlet/',
-	'Books/Henry_IV_Part_1/',
-	'Books/Henry_IV_Part_2/',
-	'Books/Henry_V/',
-	'Books/Henry_VIII/',
-	'Books/Henry_VI_Part_1/',
-	'Books/Henry_VI_Part_2/',
-	'Books/Henry_VI_Part_3/',
-	'Books/Julius_Caesar/',
-	'Books/King_John/',
-	'Books/King_Lear/',
-	'Books/Loves_Labours_Lost/',
-	'Books/Macbeth/',
-	'Books/Measure_for_Measure/',
-	'Books/Much_Ado_About_Nothing/',
-	'Books/Othello/',
-	'Books/Pericles/',
-	'Books/Richard_II/',
-	'Books/Richard_III/',
-	'Books/Romeo_and_Juliet/',
-	'Books/The_Comedy_of_Errors/',
-	'Books/The_Merchant_of_Venice/',
-	'Books/The_Merry_Wives_of_Windsor/',
-	'Books/The_Taming_of_the_Shrew/',
-	'Books/The_Tempest/',
-	'Books/The_Two_Gentlemen_of_Verona/',
-	'Books/The_Winters_Tale/',
-	'Books/Timon_of_Athens/',
-	'Books/Titus_Andronicus/',
-	'Books/Troilus_and_Cressida/',
-	'Books/Twelfth_Night/']
-
-for directory in directories:
-	csvfile_name = os.path.join(directory, 'meta_data.csv')
+for directory in os.listdir(root_dir):
+	csvfile_name = os.path.join(os.path.join(root_dir, directory), 'meta_data.csv')
 
 	print csvfile_name
 
@@ -67,12 +28,10 @@ for directory in directories:
 
 			# update the file extensions to include the full path
 		   	if row[1] != '':
-		   		print row[1]
-		   		print re.findall(re.compile(r'(character.*\.txt)'), row[1])[0]
-		   		row[1] = os.path.join(directory, re.findall(re.compile(r'(character.*\.txt)'), row[1])[0])
+		   		row[1] = row[1].replace('.txt', '_cleaned.txt')
 		   	if row[2] != '':
-		   		row[2] = os.path.join(directory, re.findall(re.compile(r'(character.*\.txt)'), row[1])[0])
+		   		row[2] = row[2].replace('.txt', '_cleaned.txt')
 		   	# rewrite the row
 		   	writer.writerow(row)
-
+	
 	shutil.move(tempfile_obj.name, csvfile_name)
