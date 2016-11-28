80ab27e8d896dc0b2668ab1ddb2160670116c7fa
Henry Aspegren <henrya@mit.edu>
2016-11-28 09:52:04 -0500

    Working version of training data routine completed

--- build_dataset.py
+++ build_dataset.py
@@ -1,12 +1,26 @@
 import SimpleVectorizer
 import numpy as np
 import pandas as pd
+import copy
 from sklearn.svm import LinearSVC
 
-simple = SimpleVectorizer.SimpleBookMatrix.getSimpleVectorizer('1984/','cleaned_embeddings_all_texts.p')
+"""
+Given a list of book objects with the vector representation methods implemented returns
+a master pandas dataframe that has all possible training and test examples 
+created from all possible pairs.
 
-def build_training_dataset(book_objects):
+"""
+def build_master_dataframe(book_objects):
 	training_data = []
+
+	pandas_data = {}
+	pandas_data['book'] = []
+	pandas_data['character_given'] = []
+	pandas_data['character_actual'] = []
+	pandas_data['book_actual'] = []
+	pandas_data['text'] = []
+	pandas_data['description'] = []
+	pandas_data['Y'] = []
 
 	# create the positive examples
 	for book in book_objects:
@@ -18,38 +32,62 @@ def build_training_dataset(book_objects):
 			combined_vec = np.concatenate((text_vec, desc_vec))
 			training_data.append(combined_vec)
 
-	single_X = np.matrix(training_data)
-	correct = np.shape(single_X)[0]
+			pandas_data['book'].append(book.book.getTitle())
+			pandas_data['character_given'].append(character)
+			pandas_data['character_actual'].append(character)
+			pandas_data['text'].append(text_vec)
+			pandas_data['description'].append(desc_vec)
+			pandas_data['book_actual'].append(book.book.getTitle())
+			# correct examples 
+			pandas_data['Y'].append(1)
 
-	# combine to produce final training matricies
-	training_X = np.copy(single_X)
+	training_df = pd.DataFrame(pandas_data)
 
-	# labels should be 1
-	training_Y = np.empty((correct, ))
-	training_Y[:] = 1
-
-	print training_Y
+	nrows = training_df.shape[0]
 
 	# create all possible negative examples from all possible pairs
-	for i in range(correct-1):
+	for i in range(1, nrows, 1):
+
 		# start from the correct matrix
-		shifted_X = np.copy(single_X)
+		shifted = copy.deepcopy(pandas_data)
 
-		# only shift the description vectors (to produce incorrect examples)
-		desc_vecs = shifted_X[: , 100:]
-		shifted = np.roll(desc_vecs, i+1, axis=0)
-		shifted_X[:, 100: ] = shifted
+		# now shift the descriptions by i+1
+		shifted['character_actual'] = shifted['character_actual'][i:] + shifted['character_actual'][:i]
+		shifted['description'] = shifted['description'][i:] + shifted['description'][:i]
+		shifted['book_actual'] = shifted['book_actual'][i:] + shifted['book_actual'][:i]
 
-		# label should be 0
-		shifted_Y = np.empty((correct, ))
-		shifted_Y[:] = 0
+		# these are all incorrect examples
+		shifted['Y'] = [0 for i in range(len(shifted['Y']))]
+		shifted_data = pd.DataFrame(shifted)
+		# want to auto-increment row indicies
+		training_df = pd.concat([training_df, shifted_data], ignore_index=True)
 
-		# add to training data
-		training_X = np.vstack((training_X, shifted_X))
-		training_Y = np.append(training_Y, shifted_Y)
+	# create the input vector to the model
+	training_df['X'] = [np.append(training_df['text'][i], training_df['description'][i]) for i in range(training_df.shape[0])]
 
-	return training_X, training_Y
+	return training_df
 
-X, Y = build_training_dataset([simple])
+
+
+"""
+Helper function for taking a pandas dataframe and 
+getting the scikit learn input numpy matricies format
+
+pandas dataframe -> numpy training matricies
+"""
+def get_numpy_matricies(pandas_df):
+	X = np.matrix(pandas_df['X'].tolist())
+	Y = np.array(pandas_df['Y'].tolist())
+
+	return X, Y
+
+
+"""
+Example Usage
+"""
+simple = SimpleVectorizer.SimpleBookMatrix.getSimpleVectorizer('1984/','cleaned_embeddings_all_texts.p')
+master_df = build_master_dataframe([simple])
+X, Y = get_numpy_matricies(master_df)
 model = LinearSVC()
 model.fit(X, Y)
+print model.score(X, Y)
