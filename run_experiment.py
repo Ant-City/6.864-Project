6b672fb6434c9f41bf46cf10faad9b14ddc6c5d0
Henry Aspegren <henrya@mit.edu>
2016-11-29 20:09:39 -0500

    Basic model implemented

--- run_experiment.py
+++ run_experiment.py
@@ -1,33 +1,53 @@
 import os, cPickle, build_dataset
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
 from Data.Book import Book
 from Models.SimpleVectorizer import SimpleBookMatrix
-
-
-
-class Experiment(object):
-
-	def __init__(self, root_dir='Books/'):
-		self.root_dir = root_dir
-
-	def __iter__(self):
-		for book_dir_name in os.listdir(self.root_dir):
-			print 'getting base model for '+book_dir_name
-			yield SimpleBookMatrix.getSimpleVectorizer(book_dir_name, 
-				'cleaned_embeddings_all_texts.p')
-
 
 #############################
 ######## Basic Model ########
 #############################
 
-# data
+# generator that uses the basic model
+class Experiment(object):
+
+	def __init__(self, root_dir='Books/'):
+		self.root_dir = root_dir
+
+	def __itir__(self):
+		for book_dir_name in os.listdir(self.root_dir):
+			print 'getting base model for '+book_dir_name
+			yield SimpleBookMatrix.getSimpleVectorizer(book_dir_name, 
+				'cleaned_embeddings_all_texts.p')
+
+# raw data
 simple_books = Experiment()
+basic_model_base = 'basic_model_base.p'
+master_df = build_dataset.build_base_dataframe(None, basic_model_base)
 
-# training_df
-training_df = build_dataset.build_master_dataframe(simple_books)
-with open(saved_file,'wb') as fp:
-	cPickle.dump(training_df, fp)  
+# vector data 
+# this distribution can be tuned
+examples_df = build_dataset.build_examples_dataframe(master_df, number_positive=3500, 
+	number_neg_same_book=3500, number_neg_diff_book=14000)
 
-print training_df
+# the Y here is simply whether the character is in the book or not
+X, Y = build_dataset.get_numpy_matricies(examples_df, correct_character=False)
+
+# partition it into training and test sets
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.1, random_state=0)
+
+# using a basic SVM
+svm_classifier = SVC()
+
+# training 
+svm_classifier.fit(X_train, Y_train)
+print 'basic model svm performance on training data: '
+print svm_classifier.score(X_train, Y_train)
+
+print '\n'
+
+# testing 
+print 'basic model svm performace on test data: '
+print svm_classifier.score(X_test, Y_test)
 
 
