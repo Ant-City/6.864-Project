73ec62d4f4beef7f79080ed527090dc4e203fd0e
Henry Aspegren <henrya@mit.edu>
2016-11-29 19:37:00 -0500

    Dataset construction finally finished

--- build_dataset.py
+++ build_dataset.py
@@ -1,17 +1,13 @@
-import os
+import os, random, copy, cPickle
 import numpy as np
 import pandas as pd
-import copy, cPickle
-from sklearn.svm import LinearSVC
-from Models.SimpleVectorizer import SimpleBookMatrix
 
 """
 Given an iterable of book objects with the vector representation methods implemented returns
-a master pandas dataframe that has correct pairings of description and text vectors. From this
-we can generate all possible pairs to get training and test data
-
+a master pandas dataframe that has correct pairings of description and text vectors along with 
+character names and book titles
 """
-def build_base_dataframe(book_objects, saved_file='basic_model_base.p'):
+def build_base_dataframe(book_objects, saved_file):
 	if os.path.isfile(saved_file):
 		print 'loading saved base dataframe...'
 		return cPickle.load(open(saved_file,'rb'))	
@@ -45,79 +41,55 @@ def build_base_dataframe(book_objects, saved_file='basic_model_base.p'):
 
 		training_df = pd.DataFrame(pandas_data)
 
-		# save it after th
+		# save it so we don't have to generate it later
 		with open(saved_file,'wb') as fp:
 			cPickle.dump(training_df, fp)  
 
 		return training_df
 
+"""
+Construct a training dataframe of examples with the input number of positive and negative examples
+"""
+def build_examples_dataframe(base_df, number_positive=10, number_neg_same_book=10, number_neg_diff_book=10):
+	copied = base_df.copy()
 
-def build_training_dataframe(base_df, number=10):
+	copied['tmp'] = 1
+
 	# text | description 
-
-	text_half = base_df.copy()[['text_book', 'text_char', 'text']]
-	desc_half = base_df.copy()[['desc_book', 'desc_char', 'desc']]
-
-	text_half['temp'] = 1
-	desc_half['temp'] = 1
+	text_half = copied[['text_book', 'text_char', 'text', 'tmp']]
+	desc_half = copied[['desc_book', 'desc_char', 'desc', 'tmp']]
 
 	# gives you the cartesian product (all possible pairings of descriptions and texts)
-	all_pairs = pd.merge(text_half, desc_half)
-	print 'mere completed'
-	# delete the temp column
-	del all_pairs['temp']
+	all_pairs = pd.merge(text_half, desc_half, on='tmp')
 
-	# sample them
-	subset = all_pairs.sample(n=(number/2), random_state=2222)
+	# delete the tmp column
+	del all_pairs['tmp']
 
-	all_pairs = base_df
+	# get the positive examples from the original dataframe
+	sampled_positives = base_df.sample(n=number_positive)
 
-	print 'here'
+	# get the negative examples from the same book (so Y_correct_book is True)
+	negative_examples_same_book = all_pairs[(all_pairs['text_book'] == all_pairs['desc_book']) & (all_pairs['text_char'] != all_pairs['desc_char'])]
+	sampled_same_negatives = negative_examples_same_book.sample(n=number_neg_same_book)
+
+	# get the negative examples from other books (so Y_correct_book is False and Y_correct_character is False)
+	negative_examples_different_book = all_pairs[(all_pairs['text_book'] != all_pairs['desc_book']) & (all_pairs['text_char'] != all_pairs['desc_char'])]
+	sampled_different_negatives = negative_examples_different_book.sample(n=number_neg_diff_book)
+
+	# all training and test data
+	final_df = pd.concat([sampled_positives, sampled_same_negatives, sampled_different_negatives], ignore_index=True)
+
 	# X
-	all_pairs['X'] = [np.append(all_pairs['text'][i], all_pairs['desc'][i]) for i in range(all_pairs.shape[0])]
-
+	final_df['X'] = [np.append(final_df['text'][i], final_df['desc'][i]) for i in final_df.index]
 	print 'X vectors created'
 
 	# Y
-	all_pairs['Y_correct_book'] = [1 if (all_pairs['text_book'][i] == all_pairs['desc_book'][i]) else 0 for i in range(all_pairs.shape[0])]
+	final_df['Y_correct_book'] = [1 if (final_df['text_book'][i] == final_df['desc_book'][i]) else 0 for i in final_df.index]
 	print 'Y correct book done'
-	all_pairs['Y_correct_character'] = [1 if (all_pairs['text_char'][i] == all_pairs['desc_char'][i]) and (all_pairs['text_book'][i] == all_pairs['desc_book'][i]) else 0 for i in range(all_pairs.shape[0])]
+	final_df['Y_correct_character'] = [1 if (final_df['text_char'][i] == final_df['desc_char'][i]) and (final_df['text_book'][i] == final_df['desc_book'][i]) else 0 for i in final_df.index]
 	print 'Y correct character'
 
-	return all_pairs
-
-
-
-def function():
-	nrows = training_df.shape[0]
-
-	# create all possible negative examples from all possible pairs
-	for i in range(1, nrows, 1):
-		if i % 5 == 0:
-			print 'negative examples '+str(i)+' out of '+str(nrows)
-		# start from the correct matrix
-		shifted = copy.deepcopy(pandas_data)
-
-		# now shift the descriptions by i+1
-		shifted['character_actual'] = shifted['character_actual'][i:] + shifted['character_actual'][:i]
-		shifted['description'] = shifted['description'][i:] + shifted['description'][:i]
-		shifted['book_actual'] = shifted['book_actual'][i:] + shifted['book_actual'][:i]
-
-		# these are all incorrect examples for characters
-		shifted['Y_correct_character'] = [0 for i in range(len(shifted['Y_correct_character']))]
-		shifted_data = pd.DataFrame(shifted)
-		# want to auto-increment row indicies
-		training_df = pd.concat([training_df, shifted_data], ignore_index=True)
-
-	# labels for books
-	training_df['Y_correct_book'] = [1 if training_df['book_actual'][i] == training_df['book'][i] else 0 for i in range(training_df.shape[0])]
-
-
-	# create the input vector to the model
-	training_df['X'] = [np.append(training_df['text'][i], training_df['description'][i]) for i in range(training_df.shape[0])]
-
-	return training_df
-
+	return final_df
 
 
 """
@@ -144,11 +116,11 @@ def get_numpy_matricies(pandas_df, correct_character=True):
 """
 Example Usage
 
-embedding_file = 'cleaned_embeddings_all_texts.p'
-simple = SimpleBookMatrix.getSimpleVectorizer('1984/',embedding_file)
-master_df = build_master_dataframe([simple])
-X, Y = get_numpy_matricies(master_df)
-model = LinearSVC()
-model.fit(X, Y)
-print model.score(X, Y)
+saved_base_df = 'basic_model_base.p'
+master_df = build_base_dataframe(None, saved_base_df)
+examples_df = build_examples_dataframe(master_df)
+X, Y = get_numpy_matricies(examples_df)
+print X
+print Y
 """
+
