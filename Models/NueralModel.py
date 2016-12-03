ac84cc268e9f3e49d4e833d849cd5e21e5454d0b
Henry Aspegren <henrya@mit.edu>
2016-12-03 14:28:59 -0500

    WIP

--- Models/NueralModel.py
+++ Models/NueralModel.py
@@ -5,39 +5,15 @@ import gensim
 from Data.Book import Book
 
 
-WORD_DIM = 100
-MAX_DESC_LENGTH = 200
-MAX_TEXT_LENGTH = 200
-NUM_HIDDEN_RNNS = 55
-NUM_HIDDEN_FINAL_NN = 99
+WORD_DIM = 10
+MAX_DESC_LENGTH = 4
+MAX_TEXT_LENGTH = 4
+NUM_HIDDEN_RNNS = 3
+NUM_HIDDEN_FINAL_NN = 2
 NUMBER_OF_OUTPUTS = 2
-
-def text_to_vector(list_of_sents, model):
-	res = np.zeros((MAX_DESC_LENGTH, WORD_DIM))
-	i = 0 
-	for sent in list_of_sents:
-		for word in sent:
-			if word in model:
-				res[i] = model[word]
-			i+=1
-			if i >= MAX_DESC_LENGTH:
-				return res
-	return res
-
-
-def weight_variable(shape):
-  initial = tf.truncated_normal(shape, stddev=0.1)
-  return tf.Variable(initial)
-
-def bias_variable(shape):
-  initial = tf.constant(0.1, shape=shape)
-  return tf.Variable(initial)
-
-
 
 # tf Graph input
 Y = tf.placeholder("float", [None, 2])
-
 
 # RNN for processing text
 with tf.variable_scope("RNN1"):
@@ -60,22 +36,22 @@ with tf.variable_scope("RNN2"):
 X = tf.concat(1, [final_state1, final_state2])
 
 
-# # NN to combine them
-W_hidden = tf.Variable(tf.zeros([NUM_HIDDEN_RNNS*2, NUM_HIDDEN_FINAL_NN]))
-b_hidden = tf.Variable(tf.zeros([1, NUM_HIDDEN_FINAL_NN]))
+## NN to combine them
+W_hidden = tf.Variable(tf.truncated_normal([NUM_HIDDEN_RNNS*2, NUM_HIDDEN_FINAL_NN], stddev=0.1), name="W_hidden")
+b_hidden = tf.Variable(tf.constant(0.1, shape=[1, NUM_HIDDEN_FINAL_NN]), name="Bias_hidden")
 
 
 hidden = tf.nn.relu(tf.matmul(X, W_hidden) + b_hidden)
 
 
-W_readout = tf.Variable(tf.zeros([NUM_HIDDEN_FINAL_NN, NUMBER_OF_OUTPUTS]))
-b_readout = tf.Variable(tf.zeros([1, NUMBER_OF_OUTPUTS]))
+W_readout = tf.Variable(tf.truncated_normal([NUM_HIDDEN_FINAL_NN, NUMBER_OF_OUTPUTS], stddev=0.1), name="W_readout")
+b_readout = tf.Variable(tf.constant(0.1, shape=[1, NUMBER_OF_OUTPUTS]), name="Bias_readout")
 
 y = tf.matmul(hidden, W_readout) + b_readout
 
 
 cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y, Y))
-train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+train_step = tf.train.AdamOptimizer().minimize(cross_entropy)
 
 correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(Y,1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
@@ -83,57 +59,77 @@ accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
 ### getting the data, feeding it in, building graph
 
-firstBook = Book.getBook('1984')
-characters =  firstBook.getCharacters()
+X_ = np.random.rand(10, MAX_DESC_LENGTH, WORD_DIM)
+X_[0:5, ]+= 2
+Y_ = np.zeros((X_.shape[0], NUMBER_OF_OUTPUTS))
+Y_[0:5, 0] = 1
+Y_[5:, 1] = 1
 
-embedding_model = cPickle.load(open('cleaned_embeddings_all_texts.p','rb'))
-
-X_ = []
-for character in characters.keys():
-	desc = characters[character]['list']
-	vec = text_to_vector(desc, embedding_model).astype(np.float32)
-	X_.append(vec)
-
-X_ = np.array(X_)
-Y_ = np.zeros((X_.shape[0], 2))
-Y_[:, 1] = 1
+print X_
 print Y_
-
 
 init = tf.global_variables_initializer()
 sess = tf.Session()
 sess.run(init)
 
+
+print "PRE-TRAINING"
+variables_names =[v.name for v in tf.trainable_variables()]
+values = sess.run(variables_names)
+for k,v in zip(variables_names, values):
+    print(k, v)
+
+
 train_accuracy = accuracy.eval(session=sess, feed_dict={x1:X_, x2:X_, Y:Y_})
 print 'initial training accruacy '+ str(train_accuracy)
+
 train_step.run(session=sess, feed_dict={x1:X_, x2:X_, Y:Y_})
+print "POST TRAINING"
+
+variables_names =[v.name for v in tf.trainable_variables()]
+values = sess.run(variables_names)
+for k,v in zip(variables_names, values):
+    print(k, v)
+
 train_accuracy = accuracy.eval(session=sess, feed_dict={x1:X_, x2:X_, Y:Y_})
 print 'final training accruacy '+ str(train_accuracy)
  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# summary_writer = tf.train.SummaryWriter('/Users/henryaspegren/Dropbox (MIT)/Academics/MIT/Senior Fall (2016)/6.864/Project/6.864-Project/log/', sess.graph)
+
 
  #out = sess.run(correct_prediction, feed_dict={x1:X_, x2:X_, Y:Y_})
 # print final_output[0].shape
 # print final_output[0]
 # print final_output[1].shape
 # print final_output[1]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
